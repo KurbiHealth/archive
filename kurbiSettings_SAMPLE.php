890f2b8c249f5c5678bbb02aa651003cab59bfbf
Matt Eckman <matteckman@gmail.com>
2015-04-30 16:24:31 -0400

    adding zipped repositories from beta

---
+++ kurbiSettings_SAMPLE.php
@@ -0,0 +1,9 @@
+<?php
+
+if(!defined('ROOT_URL')) 
+	define('ROOT_URL','http://ksupporter:8888');
+
+if(!defined('SERVER_ROOT_PATH'))
+	define('SERVER_ROOT_PATH','/Applications/MAMP/htdocs/dev/KURBI/friendsfamily/');
+
+?>
